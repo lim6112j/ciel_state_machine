b8994523aa9cc55ab9e4a54bb9c5cbc9f65d35ba
ben lim <lim6112j@gmail.com>
2024-08-19 23:59:22 +0900

    api modified, restclient code

--- lib/ciel_state_machine/api.ex
+++ lib/ciel_state_machine/api.ex
@@ -4,7 +4,7 @@ defmodule CielStateMachine.Api do
 	plug(Plug.Parsers, parsers: [:json], pass: ["application/json"], json_decoder: Poison)
 	plug :dispatch
 	@port 5454
-	
+
 	def child_spec(_arg) do
 		Plug.Cowboy.child_spec(
 			scheme: :http,
@@ -16,13 +16,19 @@ defmodule CielStateMachine.Api do
 	post "/supply" do
 		{:ok, _, conn} = Plug.Conn.read_body(conn)
 		IO.puts "\n encoded body: #{inspect(conn.params)}"
-		name = Map.fetch!(conn.params, "name")
-		name
+		supply_idx = Map.fetch!(conn.params, "supply_idx")
+		vehicle_plate_num = Map.fetch!(conn.params, "vehicle_plate_num")
+		CielStateMachine.ProcessFactory.server_process(supply_idx)
+		CielStateMachine.Server.add_entry(supply_idx, %{supply_idx: supply_idx, vehicle_plate_num: vehicle_plate_num})
+		conn
 		|> Plug.Conn.send_resp(200, "OK")
 	end
 	get "/supplies" do
+		supply_idx = Map.fetch!(conn.params, "supply_idx")
+		IO.puts "supply idx from get request = #{supply_idx}"
+		entries = CielStateMachine.Server.entries(supply_idx)
 		conn
-		|> Plug.Conn.send_resp(200, Poison.encode!( %{response: "hello ciel"} ))
+		|> Plug.Conn.send_resp(200, Poison.encode!( %{response: entries} ))
 	end
 
 end

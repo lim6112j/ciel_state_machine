448e5184f6bd1b53263b805092f83da27b227e45
ben lim <lim6112j@gmail.com>
2024-08-22 10:43:03 +0900

    test added

--- test/ciel_state_machine_test.exs
+++ test/ciel_state_machine_test.exs
@@ -1,8 +1,25 @@
 defmodule CielStateMachineTest do
   use ExUnit.Case
-  doctest CielStateMachine
 
-  test "greets the world" do
-    assert CielStateMachine.hello() == :world
+  test " processfactory create server" do
+    assert is_pid(CielStateMachine.ProcessFactory.server_process(1)) == true
   end
+	test "state machine create server when needed" do
+		CielStateMachine.Store.dispatch(%{type: "ADD_VEHICLE"}, 1)
+		regs = Registry.select(CielStateMachine.ProcessRegistry, [{{:"$1", :_, :_}, [], [:"$1"]}])
+		assert length(regs) == 1
+	end
+	test "state machine subscriber" do
+		CielStateMachine.Store.subscribe(fn state ->
+			IO.puts "subscriber got called with state #{inspect(state)}"
+			CielStateMachine.Server.update_location(2, %{lng: 124, lat: 37})
+			assert CielStateMachine.Server.get_state(2) |> Map.fetch(:current_loc) |> Map.fetch(:lng) == 124
+		end
+		)
+		CielStateMachine.Store.dispatch(%{type: "ADD_VEHICLE"}, 2 ) # supply_idx 1 vehicle add
+
+
+	end
+
+
 end

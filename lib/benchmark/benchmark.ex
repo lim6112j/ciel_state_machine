facc1c012e972167dc54708e12f7c33648647d8a
ben lim <lim6112j@gmail.com>
2024-09-24 19:35:47 +0900

    info -> warn for benchmark

--- lib/benchmark/benchmark.ex
+++ lib/benchmark/benchmark.ex
@@ -4,7 +4,7 @@ defmodule Benchmark do
 		num_cars = Keyword.get(opts, :num_cars, 10000)
 		concurrency = min(Keyword.get(opts, :concurrency, 8), num_cars)
 		items_per_process = div(num_cars, concurrency)
-		Logger.info "benchmark testing ..."
+		Logger.warn "benchmark testing ..."
 		{time, _} =
 			:timer.tc(fn ->
 				0..(concurrency - 1)
@@ -14,8 +14,8 @@ defmodule Benchmark do
 		# for checkk log uncomment below
 		# Process.sleep(1000)
 		throughput = round(num_cars * 1_000_000 /time)
-		Logger.info "\n\n #### car dispatch time spent #{time / 1000} miliseconds"
-		Logger.info "\n\n #### car dispatch throughput: #{throughput} operations/sec \n"
+		Logger.warn "\n\n #### car dispatch time spent #{time / 1000} miliseconds"
+		Logger.warn "\n\n #### car dispatch throughput: #{throughput} operations/sec \n"
 		{loc_time, _} =
 			:timer.tc(fn ->
 				0..(concurrency  - 1)
@@ -25,8 +25,8 @@ defmodule Benchmark do
 		# for checkk log uncomment below
 		# Process.sleep(5000)
 		update_throughput = round(num_cars * 1_000_000 / loc_time)
-		Logger.info "\n\n ### car loc updated time spent #{loc_time / 1000} milliseconds"
-		Logger.info "\n\n #### car loc updated throughput #{update_throughput} operations/sec \n"
+		Logger.warn "\n\n ### car loc updated time spent #{loc_time / 1000} milliseconds"
+		Logger.warn "\n\n #### car loc updated throughput #{update_throughput} operations/sec \n"
 		# sleep for cast methods to finish jobs
 		Process.sleep(10000)
 		{call_time, _} =
@@ -36,8 +36,8 @@ defmodule Benchmark do
 				|> Enum.map(&Task.await(&1, :infinity))
 			end)
 		call_throughput = round(num_cars * 1_000_000 /call_time)
-		Logger.info "\n\n #### car get state call time spent #{call_time / 1000} miliseconds"
-		Logger.info "\n\n #### car get state call  call_throughput: #{call_throughput} operations/sec \n"
+		Logger.warn "\n\n #### car get state call time spent #{call_time / 1000} miliseconds"
+		Logger.warn "\n\n #### car get state call  call_throughput: #{call_throughput} operations/sec \n"
 
 	end
 	defp performBench(start_item, items_per_process) do
@@ -74,7 +74,7 @@ defmodule Benchmark do
 	defp get_state(end_item, end_item), do: :ok
 	defp get_state(start_item, end_item) do
 		state = CielStateMachine.Server.get_state(start_item)
-		# Logger.info "supply_id : #{start_item}, state: #{inspect(state)}"
+		# Logger.warn "supply_id : #{start_item}, state: #{inspect(state)}"
 		get_state(start_item + 1, end_item)
 	end
 

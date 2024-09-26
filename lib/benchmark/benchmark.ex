f8ef8c9c4f0d3a1b11410570cf232c1208280ee2
ben lim <lim6112j@gmail.com>
2024-09-26 10:42:32 +0900

    updates num added for benchmark

--- lib/benchmark/benchmark.ex
+++ lib/benchmark/benchmark.ex
@@ -3,8 +3,9 @@ defmodule Benchmark do
 	def run( opts \\ []) do
 		num_cars = Keyword.get(opts, :num_cars, 10000)
 		concurrency = min(Keyword.get(opts, :concurrency, 8), num_cars)
+		num_updates = Keyword.get(opts, :num_updates, 10)
 		items_per_process = div(num_cars, concurrency)
-		Logger.warn "benchmark testing ..."
+		Logger.warn "benchmark testing ... num_cars: #{num_cars}, concurrency: #{concurrency}, num_updates: #{num_updates}, items_per_process: #{items_per_process}"
 		{time, _} =
 			:timer.tc(fn ->
 				0..(concurrency - 1)
@@ -19,16 +20,16 @@ defmodule Benchmark do
 		{loc_time, _} =
 			:timer.tc(fn ->
 				0..(concurrency  - 1)
-				|> Enum.map(&performUpdateBench(&1 * items_per_process, items_per_process))
+				|> Enum.map(&performUpdateBench(&1 * items_per_process, items_per_process, num_updates))
 				|> Enum.map(&Task.await(&1, :infinity))
 			end)
 		# for checkk log uncomment below
 		# Process.sleep(5000)
-		update_throughput = round(num_cars * 1_000_000 / loc_time)
+		update_throughput = round(num_cars * 1_000_000 * num_updates / loc_time)
 		Logger.warn "\n\n ### car loc updated time spent #{loc_time / 1000} milliseconds"
 		Logger.warn "\n\n #### car loc updated throughput #{update_throughput} operations/sec \n"
 		# sleep for cast methods to finish jobs
-		Process.sleep(2000)
+		Process.sleep(10000)
 		{call_time, _} =
 			:timer.tc(fn ->
 				0..(concurrency - 1)
@@ -52,18 +53,24 @@ defmodule Benchmark do
 		add_vehicle(start_item + 1, end_item)
 	end
 
-	defp performUpdateBench(start_item, items_per_process) do
+	defp performUpdateBench(start_item, items_per_process, num_updates) do
 		Task.async(fn ->
-			update_vehicle(start_item, start_item + items_per_process)
+			update_vehicles(start_item, start_item + items_per_process, num_updates)
 		end)
 	end
-	defp update_vehicle(end_item, end_item), do: :ok
-	defp update_vehicle(start_item, end_item) do
+	defp update_vehicles(_start_item, _end_item, 0), do: :ok
+	defp update_vehicles(start_item, end_item, updates) do
+		update_vehicle(start_item, end_item , updates)
+		update_vehicles(start_item, end_item, updates - 1)
+	end
+
+	defp update_vehicle(end_item, end_item, updates), do: :ok
+	defp update_vehicle(start_item, end_item, updates) do
 		CielStateMachine.Store.dispatch(
 			%{type: "UPDATE_CAR_LOCATION"} ,
 			[{start_item, %{lng: 127.1, lat: 37.1}}]
 		)
-		update_vehicle(start_item + 1, end_item)
+		update_vehicle(start_item + 1, end_item, updates)
 	end
 
 	defp performGetStateCall(start_item, items_per_process) do
